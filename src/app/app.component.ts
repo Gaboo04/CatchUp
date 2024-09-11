96cf8202929cc3eac74484ff343257fc292d8c52
Gabo <u20201e889@upc.edu.pe>
2024-09-11 00:59:48 -0500

    fix: add pending component import.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TranslateService} from "@ngx-translate/core";
 import {SideNavigationBarComponent} from "./public/components/side-navigation-bar/side-navigation-bar.component";
+import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideNavigationBarComponent],
+  imports: [RouterOutlet, SideNavigationBarComponent, FooterContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/public/components/language-switcher/language-switcher.component.ts
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
 import {group} from "@angular/animations";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-language-switcher',
   standalone: true,
   imports: [
     MatButtonToggleGroup,
-    MatButtonToggle
+    MatButtonToggle,
+    NgForOf
   ],
   templateUrl: './language-switcher.component.html',
   styleUrl: './language-switcher.component.css'
